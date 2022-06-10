a4a6e693c433cb51dc412877413da0511873ee9b
Santiagogure <102995817+Santiagogure@users.noreply.github.com>
2022-06-10 11:24:44 -0300

    Add files via upload

---
+++ splice y slice/main.js
@@ -0,0 +1,19 @@
+//Splice
+
+const nombres = ['Ana', 'Javier', 'Maru', 'Luis']
+
+//nombres.splice(index, cantidad)  //index es la posicion donde queremos agregar, o eliminar. El segundo es la cantidad de elementos que queremos agregar o eliminar 
+
+nombres.splice(2, 0, 'Jose')   // Con esto, Jose se posicionaria en medio de "Javier-Maru"
+nombres.splice(2, 1, 'Jose')   // Con esto, "Maru" seria eliminado
+
+console.log('nombres')
+
+
+//Slice
+slice(comienza, termina)  //Estos son sus dos argumentos
+
+const nombres2 = nombres.slice(0, 2)  //Teoricamente, el 2 deberia terminar en "Maru", pero el segundo argumento no cuenta, asi que conn esto queda ["Ana", "Javier"]
+const nombres3 = nombres.slice(2, 4)  // Con esto obtenemos a maru y luis
+
+//Diferencias: Slice no modifica el arreglo original
