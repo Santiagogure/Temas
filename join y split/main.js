f84fd81b486877d667b206bcdab2b86c3c0b0812
Santiagogure <102995817+Santiagogure@users.noreply.github.com>
2022-06-23 21:27:34 -0300

    Add files via upload

---
+++ join y split/main.js
@@ -0,0 +1,20 @@
+// Join
+
+let array = ["H", "o", "l", "a"];
+
+let arrayPasado = array.join("")
+
+console.log(arrayPasado) // De esta forma nos devuelve el hola.
+
+let arrayUnido = ["Hola", "Como", "Estas"]
+
+let arrayJunto = arrayUnido.join(" ") //Como aca tenemos palabras, para que no se muestre todo junto ponemos un espacio
+
+// ("") En las comillas podemos poner "," o todo tipo de 
+
+//Recordar que su contrario es split
+
+let texto = "Hola como estas"
+let textoDividido = texto.split(" ")
+console.log(textoDividido)    
+ 
