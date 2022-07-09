b335f33152a8fbe7e99f43dbfd3cc97edfc83c8f
Santiagogure <102995817+Santiagogure@users.noreply.github.com>
2022-07-09 16:16:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+
+const moduloMatematicas = require("./matematicas.js")
+const matematicas = moduloMatematicas.factorial
+const {factorial} = moduloMatematicas
+
+const sum = moduloMatematicas.suma(12,90)
+console.log(sum)
