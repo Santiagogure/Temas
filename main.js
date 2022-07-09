d054782a93b2f6539767182b7028c7c042130a12
Santiagogure <102995817+Santiagogure@users.noreply.github.com>
2022-07-09 16:17:15 -0300

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-
-const moduloMatematicas = require("./matematicas.js")
-const matematicas = moduloMatematicas.factorial
-const {factorial} = moduloMatematicas
-
-const sum = moduloMatematicas.suma(12,90)
-console.log(sum)
