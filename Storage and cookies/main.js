e0355d459ccc46c4ab2421c5e5864b8a43fbafb9
Santiagogure <102995817+Santiagogure@users.noreply.github.com>
2022-07-18 11:12:44 -0300

    Add files via upload

---
+++ Storage and cookies/main.js
@@ -0,0 +1,16 @@
+// var nombre = "Santiago"
+// var apellido = "Gurevich"
+
+
+
+
+// sessionStorage.setItem("nombreCompleto", JSON.stringify( {name: nombre, subname: apellido}))
+// console.log(JSON.parse(sessionStorage.getItem("nombreCompleto")))
+
+// localStorage.setItem("nombreCompleto", JSON.stringify({name: nombre, subname: apellido}))
+// console.log(JSON.parse(localStorage.getItem("nombreCompleto")))
+
+// document.cookie = "nombreCookie=SantiagoCookie"
+
+// document.cookie = "nombreExpira=Nombre;expires=" + new Date(2022, 6, 18, 0, 2,).toUTCString
+// console.log(document.cook)
